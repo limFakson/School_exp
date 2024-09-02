04b6a48d4be41fdc6e374bd5ab250ae31db85202
limfakson <fakeyejoshua2005@gmail.com>
2024-09-02 22:54:42 +0100

    test practice

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String make = "chevrolete";
+    String model = "porshe";
+    int year = 2024;
+
+    public void drive(){
+        System.out.println("we are driving");
+    }
+
+    public void brake(){
+        System.out.println("brake effected");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
+// import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
+    float g = 33.2676767f;
+    byte e = 127;
     char c = 'y';
     int n = 3;
-    long l = 120;
-    double g = 3.2;
+    long l = 120_8922389_38982l;
+ 
 
-    String exchange = addString("People");
+    System.out.println(e);
 
-    System.out.println( exchange + " " + c + " " + n + " " + l+ " " + g + " " + "we done!");
+    String Exchange = addString("People");
+
+    System.out.println( Exchange + " " + c + " " + n + " " + l+ " " + g + " " + "we done!");
+    
+    Car mycar = new Car();
+    Car mycar2 = mycar;
+
+    System.out.println(mycar.make);
+    System.out.println(mycar2.model);
+
   }
 
   public static String addString(String s){
